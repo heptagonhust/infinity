8ede1bf37acfb6da9914301a2e547338d2637437
Recolic Keghart <root@recolic.net>
2018-09-10 20:51:51 +0800

    update interface but still considering read/write logic

--- src/infinity/java-wrapper/RdmaNative.java
+++ src/infinity/java-wrapper/RdmaNative.java
@@ -2,17 +2,42 @@ package org.apache.hadoop.hbase.ipc;
 
 import java.nio.ByteBuffer;
 public class RdmaNative {
-    public native boolean rdmaIsOpen(Object RdmaConnection);
-    public native Object rdmaConnect(int port,String addr);
-    public native Object rdmaBlockedAccept(int port);
-    public native int rdmaClose(Object RdmaConnection);
-    public native ByteBuffer rdmaRead(Object RdmaConnection);
-    public native boolean rdmaReadable(Object RdmaConnection);
-    public native boolean rdmaWrite(Object RdmaConnection, ByteBuffer sbuf);
-    public native boolean rdmaRespond(Object RdmaConnection, ByteBuffer sbuf);
+    // This function must be called exactly once to construct necessary structs.
+    // It will construct rdmaContext and other global var.
+    public native int rdmaInitGlobal();
+    // This function must be called exactly once to destruct global structs.
+    public native int rdmaDestroyGlobal();
+
+    // Connect to remote host. Blocked operation. If success, returnedConn.errorCode holds 0.
+    public native RdmaConnection rdmaConnect(String addr, int port);
+    // Wait and accept a connection. Blocked operation. If success, returnedConn.errorCode holds 0.
+    public native RdmaConnection rdmaBlockedAccept(int port);
 
     public class RdmaConnection {
         private long ptrQP;
-        private long ptrRbuf;
+        private long ptrRegionTokenBuf; // registered as fixed length while making conn.
+        private long ptrDynamicDataBuf; // must register on every local write. Invalidate it only if ptrRegionTokenBuf has changed!
+        private boolean isServer; 
+        private int errorCode;
+        private boolean isClosed;
+
+        // check if this connection is closed.
+        public boolean isClosed() {
+            return this.isClosed;
+        }
+        // true if is server, false if is client.
+        public boolean isServer() {
+            return this.isServer;
+        }
+        // Only used for rdmaConnect and rdmaBlockedAccept.
+        public int getErrorCode() {
+            return this.errorCode;
+        }
+        // Assume remote holds only one buffer. Read the buffer. Blockd call.
+        public native ByteBuffer readRemote();
+        // Use JNI GlobalRef to prevent GC, register this buffer, and replace ptrRegionTokenBuf atomicly for peer read. Blocked call.
+        public native int writeLocal(ByteBuffer data); 
+        public native boolean isRemoteReadable(); 
+        public native int close();
     }
 }
--- src/infinity/java-wrapper/org_apache_hadoop_hbase_ipc_RdmaNative.h
+++ src/infinity/java-wrapper/org_apache_hadoop_hbase_ipc_RdmaNative.h
@@ -9,59 +9,35 @@ extern "C" {
 #endif
 /*
  * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
- * Method:    rdmaIsOpen
- * Signature: (Ljava/lang/Object;)Z
+ * Method:    rdmaInitGlobal
+ * Signature: ()I
  */
-JNIEXPORT jboolean JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaIsOpen(JNIEnv *, jobject, jobject);
+JNIEXPORT jint JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaInitGlobal
+  (JNIEnv *, jobject);
+
+/*
+ * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
+ * Method:    rdmaDestroyGlobal
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaDestroyGlobal
+  (JNIEnv *, jobject);
 
 /*
  * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
  * Method:    rdmaConnect
- * Signature: (ILjava/lang/String;)Ljava/lang/Object;
+ * Signature: (Ljava/lang/String;I)Lorg/apache/hadoop/hbase/ipc/RdmaNative/RdmaConnection;
  */
-JNIEXPORT jobject JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaConnect(JNIEnv *, jobject, jint, jstring);
+JNIEXPORT jobject JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaConnect
+  (JNIEnv *, jobject, jstring, jint);
 
 /*
  * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
  * Method:    rdmaBlockedAccept
- * Signature: (I)Ljava/lang/Object;
+ * Signature: (I)Lorg/apache/hadoop/hbase/ipc/RdmaNative/RdmaConnection;
  */
-JNIEXPORT jobject JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaBlockedAccept(JNIEnv *, jobject, jint);
-
-/*
- * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
- * Method:    rdmaClose
- * Signature: (Ljava/lang/Object;)I
- */
-JNIEXPORT jint JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaClose(JNIEnv *, jobject, jobject);
-
-/*
- * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
- * Method:    rdmaRead
- * Signature: (Ljava/lang/Object;)Ljava/nio/ByteBuffer;
- */
-JNIEXPORT jobject JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaRead(JNIEnv *, jobject, jobject);
-
-/*
- * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
- * Method:    rdmaReadable
- * Signature: (Ljava/lang/Object;)Z
- */
-JNIEXPORT jboolean JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaReadable(JNIEnv *, jobject, jobject);
-
-/*
- * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
- * Method:    rdmaWrite
- * Signature: (Ljava/lang/Object;Ljava/nio/ByteBuffer;)Z
- */
-JNIEXPORT jboolean JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaWrite(JNIEnv *, jobject, jobject, jobject);
-
-/*
- * Class:     org_apache_hadoop_hbase_ipc_RdmaNative
- * Method:    rdmaRespond
- * Signature: (Ljava/lang/Object;Ljava/nio/ByteBuffer;)Z
- */
-JNIEXPORT jboolean JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaRespond(JNIEnv *, jobject, jobject, jobject);
+JNIEXPORT jobject JNICALL Java_org_apache_hadoop_hbase_ipc_RdmaNative_rdmaBlockedAccept
+  (JNIEnv *, jobject, jint);
 
 #ifdef __cplusplus
 }
