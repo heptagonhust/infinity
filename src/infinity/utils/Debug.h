0dc24ca95935a6e0d02171c7191ff11fb50a2293
Recolic Keghart <root@recolic.net>
2018-09-16 12:50:08 +0800

    sync

--- src/infinity/utils/Debug.h
+++ src/infinity/utils/Debug.h
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <exception>
+#include <stdexcept>
 
 #ifdef INFINITY_DEBUG_ON
 	#define INFINITY_DEBUG(X, ...) {fprintf(stdout, X, ##__VA_ARGS__); fflush(stdout);}
@@ -20,7 +21,7 @@
 #endif
 
 #ifdef INFINITY_ASSERT_ON
-	#define INFINITY_ASSERT(B, X, ...) {if(!(B)) {fprintf(stdout, X, ##__VA_ARGS__); fflush(stdout); throw std::exception("Infinity assertion error.");}}
+	#define INFINITY_ASSERT(B, X, ...) {if(!(B)) {fprintf(stdout, X, ##__VA_ARGS__); fflush(stdout); throw std::runtime_error("Infinity assertion error.");}}
 #else
 	#define INFINITY_ASSERT(B, X, ...) {}
 #endif
