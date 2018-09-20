b989247246f45ff831303df1a00ec7316e7f5486
Recolic Keghart <root@recolic.net>
2018-09-21 00:52:24 +0800

    remote_debug

--- src/infinity/queues/QueuePairFactory.cpp
+++ src/infinity/queues/QueuePairFactory.cpp
@@ -95,14 +95,14 @@ QueuePair * QueuePairFactory::acceptIncomingConnection(void *userData, uint32_t 
     { // recolic: peer addr
         char cliIpStr[INET6_ADDRSTRLEN];
         // deal with both IPv4 and IPv6:
-        if (addr.ss_family == AF_INET) {
-            struct sockaddr_in *s = (struct sockaddr_in *)&addr;
-            port = ntohs(s->sin_port);
-            inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+        if (cliAddr.ss_family == AF_INET) {
+            struct sockaddr_in *s = (struct sockaddr_in *)&cliAddr;
+            int port = ntohs(s->sin_port);
+            inet_ntop(AF_INET, &s->sin_addr, cliIpStr, sizeof cliIpStr);
         } else { // AF_INET6
-            struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
-            port = ntohs(s->sin6_port);
-            inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
+            struct sockaddr_in6 *s = (struct sockaddr_in6 *)&cliAddr;
+            int port = ntohs(s->sin6_port);
+            inet_ntop(AF_INET6, &s->sin6_addr, cliIpStr, sizeof cliIpStr);
         }
         queuePair->peerAddr = cliIpStr;
     }
