c89551c97bc663dd14e2fb4a388ca5823c856aef
Recolic Keghart <root@recolic.net>
2018-09-30 00:45:37 +0800

    sync

--- src/infinity/core/Context.cpp
+++ src/infinity/core/Context.cpp
@@ -186,7 +186,7 @@ bool Context::pollSendCompletionQueue() {
 		if (wc.status == IBV_WC_SUCCESS) {
 			INFINITY_DEBUG("[INFINITY][CORE][CONTEXT] Request completed (id %lu).\n", wc.wr_id);
 		} else {
-			INFINITY_DEBUG("[INFINITY][CORE][CONTEXT] Request failed (id %lu errno %d status %d).\n", wc.wr_id, errno, wc.status);
+			INFINITY_DEBUG("[INFINITY][CORE][CONTEXT] Request failed (id %lu errno %d status %d wc.vendor_err=%u).\n", wc.wr_id, errno, wc.status, wc.vendor_err);
             throw std::runtime_error("[INFINITY][CORE][CONTEXT] Request failed. read the log.");
 		}
 		return true;
