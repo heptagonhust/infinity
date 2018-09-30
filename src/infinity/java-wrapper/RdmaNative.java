9710f18df51205aacc71f2fae52480243c4976d4
Recolic Keghart <root@recolic.net>
2018-09-30 08:18:37 +0800

    update readme and interface comment

--- src/infinity/java-wrapper/RdmaNative.java
+++ src/infinity/java-wrapper/RdmaNative.java
@@ -40,27 +40,26 @@ public class RdmaNative {
             and DynamicBuffer holds the real query or response data. 
             In fact, DynamicBufferTokenBuffer is the current "status" of server. Its "magic" is the current state of a state machine. 
 
-        The Magic is inited to 0x00000000 and set to 0xffffffff once DynamicBufferToken is ready for using.
+            The Magic is inited to MAGIC_CONNECTED and set to MAGIC_SERVER_BUFFER_READY once DynamicBufferToken is ready for using.
             On accepting connection, the server creates a 4K DynamicBuffer, and register it, put the token into DynamicBufferTokenBuffer.
         begin:
             Then the server send the DynamicBufferTokenBufferToken to client as userData. Then the client write its querySize into 
             DynamicBufferTokenBuffer. On server calling isQueryReadable(), it allocates the buffer if querySize available.
             If the client's query size is less than current DynamicBufferSize, it just write it in the existing dynamic buffer. 
-            If the query is larger, the client must read DynamicBufferTokenBuffer again and again, until the Magic is NOT 0x00000000. 
-            Then write the query in.
+            If the query is larger, the client must read server magic again and again, until the Magic is MAGIC_SERVER_BUFFER_READY. 
+            Then write its query in.
             If the query is larger, the server must resize and re-register the DynamicBuffer, put its new token into 
-            DynamicBufferTokenBuffer, and set the Magic to 0xffffffff.
+            DynamicBufferTokenBuffer, and set the Magic to MAGIC_SERVER_BUFFER_READY.
             
-            Once the query is wrote into server, the client must set Magic to 0xaaaaaaaa(use compareAndSwap, if possible. It doesn't matter).
-            The server call isQueryReadable() to check if the Magic is 0xaaaaaaaa. 
+            Once the query is wrote into server, the client must set Magic to MAGIC_QUERY_WROTE. The server call isQueryReadable() to check 
+            if the Magic is MAGIC_QUERY_WROTE. After reading it, the server set its magic to MAGIC_QUERY_READ to avoid duplicated QueryReadable.
 
-            After the response is ready, the server use writeResponse to write its data to local buffer. (In fact, the memory in ByteBuffer maybe 
-            used and prevent its GC). The DynamicBufferToken is updated, then the Magic is set to 0x55555555. The client check the value again
-            and again. Once available, the client can get the response.
+            After the response is ready, the server use writeResponse to write its data to local buffer. The DynamicBuffer will be reallocated if 
+            its not large enough. The DynamicBufferToken is updated, then the Magic is set to MAGIC_RESPONSE_READY. The client check the value again
+            and again before reading the response by RDMA.
 
-            I have not consider how should I close the connection. Maybe just destruct the Rdma*Connection, and destruct the insiding C++ QP. 
-            If we want to reuse the connection in the future, just leave the connection there and the server use current DynamicBuffer as initial
-            buffer, goto begin;
+            On closing a connection, just destruct the Rdma*Connection, and destruct the insiding C++ QP. 
+            If we want to reuse the connection, just `goto begin` and everything works perfectly!
         */
         private long ptrCxxClass;
 
